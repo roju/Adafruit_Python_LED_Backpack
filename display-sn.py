cf06978ef016061405f31efc086a736c1f5e61de
Ross Justin <justrostin@gmail.com>
2018-07-27 17:14:59 -0700

    added script

---
+++ display-sn.py
@@ -0,0 +1,18 @@
+import time
+
+from Adafruit_LED_Backpack import SevenSegment
+
+
+# Create display instance on default I2C address (0x70) and bus number.
+display = SevenSegment.SevenSegment()
+
+# Alternatively, create a display with a specific I2C address and/or bus.
+# display = SevenSegment.SevenSegment(address=0x74, busnum=1)
+
+# Initialize the display. Must be called once before using the display.
+display.begin()
+
+display.clear()
+display.print_float(155, decimal_digits=1)
+display.set_colon(False)
+display.write_display()
